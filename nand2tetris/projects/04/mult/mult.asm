2cb35b6d297015543b79160be5a377d882f22213
3dprintscanner <1988anthonythomas@gmail.com>
2017-08-22 21:02:02 +0100

    Assembler multiplier works

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,63 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//x*y // var sum = 0 // for(i=0;i<=y;++i){//   sum = sum + x //} //return x;
+
+// set memory[2] to 0
+
+
+
+//set i = 0
+	@i
+	M=0
+
+	@ARG
+	M=0
+
+	// check if either inputs are zero
+	
+	@SP
+	D=M
+	@END
+	D;JEQ
+
+	@LCL
+	D=M
+	@END
+	D;JEQ
+
+(LOOP)
+	// check i <=y e.g y - i > 0, use JLE 
+	
+	// set input 0 to D register
+	@LCL
+	D=M
+
+	// compute i value
+	@i
+	D=D-M
+
+
+	@END
+	D;JLE
+
+	// increment i
+	@i
+	M=M+1
+	// multiply numbers by addition
+	@SP
+	D=M
+
+	@ARG
+	M=D+M
+	
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
+
+
+
+
+
