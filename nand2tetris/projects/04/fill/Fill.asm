61152df0a7f9b3625d672c1604bff277973598b2
3dprintscanner <1988anthonythomas@gmail.com>
2017-08-22 22:15:18 +0100

    Screen fill assembler code complete

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,117 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+// check if any key is pressed
+
+
+
+// while(true){
+//	var key = getKeyVal();
+//  if(key != 0){
+//		SCREEN = -1
+//}	
+//}
+
+
+	
+
+(LOOP)
+	
+	@24576
+	D=M
+	@CLEARSCREEN
+	D;JEQ
+
+
+
+	// requires 8192 writes
+	@iclear
+	M=0
+	
+	@8192
+	D=A
+	@limclear  // save max into value
+	M=D
+
+	@16384
+	D=A
+	
+	@currentclear // save current memory address to some value
+	M=D
+
+	@i
+	M=0
+	
+	@8192
+	D=A
+	@lim  // save max into value
+	M=D
+
+	@16384
+	D=A
+	
+	@current // save current memory address to some value
+	M=D
+
+(WRITESCREEN)
+	// if i >= 8192 then jump to start
+	
+	// check if key still pressed
+
+	@24576
+	D=M
+	@CLEARSCREEN
+	D;JEQ
+
+	@lim
+	D=M
+
+	@i
+	D=D-M // 8192 - i
+
+	@LOOP
+	D;JLE // jump if less than
+
+	@current
+	A=M
+	M=-1
+	
+	@i
+	M=M+1 // increment i
+
+	@current
+	M=M+1 // increment current
+
+	@WRITESCREEN
+	0;JMP
+
+
+
+
+(CLEARSCREEN)
+
+	@limclear
+	D=M
+
+	@iclear
+	D=D-M // 8192 - i
+
+	@LOOP
+	D;JLE // jump if less than
+
+	@currentclear
+	A=M
+	M=0
+	
+	@iclear
+	M=M+1 // increment i
+
+	@currentclear
+	M=M+1 // increment current
+
+	@CLEARSCREEN
+	0;JMP
+
+(END)
